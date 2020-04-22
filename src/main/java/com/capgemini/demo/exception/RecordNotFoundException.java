b39029185d0e8834e648ac2377b248a8d45d81ad
mahanidhib <mahanidhi125@gmail.com>
2020-04-22 14:00:11 +0530

    first commit

--- src/main/java/com/capgemini/demo/exception/RecordNotFoundException.java
+++ src/main/java/com/capgemini/demo/exception/RecordNotFoundException.java
@@ -10,22 +10,3 @@ public class RecordNotFoundException extends RuntimeException {
         super(message, cause);
     }
 }
-
-
-/*package com.example.test.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "RecordNotFoundException not found")
-public class RecordNotFoundException extends RuntimeException {
-	
-	
-    public RecordNotFoundException(String message) {
-        super(message);
-    }
-
-    public RecordNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}*/
--- src/main/java/com/capgemini/demo/service/GeoService.java
+++ src/main/java/com/capgemini/demo/service/GeoService.java
@@ -52,73 +52,10 @@ public class GeoService {
 		return addAllRecords(update);
 
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-/*package com.example.test.service;
-
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.example.test.exception.RecordNotFoundException;
-import com.example.test.model.UomConversionDated;
-import com.example.test.repositry.DatedConversionToUomRepositry;
-import com.example.test.repositry.DatedMainUomConversionDatedRepositry;
-import com.example.test.repositry.UomConversionDatedRepositry;
-
-@Service
-public class CommonEntiesService {
-
-	@Autowired
-	UomConversionDatedRepositry uomRepositry;
-	@Autowired
-	DatedMainUomConversionDatedRepositry dmcdRepositry;
-	@Autowired
-	DatedConversionToUomRepositry dctuRepositry;
-
-	public UomConversionDated addAllRecords(UomConversionDated dated) {
-		return uomRepositry.save(dated);
-	}
-
-	public List<UomConversionDated> findAllRecords() {
-		return uomRepositry.findAll();
-	}
-
-	public UomConversionDated fetchById(int id) {
-		return uomRepositry.findById(id).orElseThrow(
-				() -> new RecordNotFoundException("Record is not present with id " + id + " check with your Database"));
-	}
-
-	public String deleteId(int id) {
-		String records = uomRepositry.findById(id).map(uom -> {
-			uomRepositry.delete(uom);
-			return "Delete Successfully!";
-		}).orElseThrow(
-				() -> new RecordNotFoundException("Record is not present with id " + id + " check with your Database"));
-		return records;
-	}
-
-	public UomConversionDated updateId(int id, UomConversionDated updateDetails) {
-		UomConversionDated update = fetchById(id);
-		update.setUomIdTo(updateDetails.getUomIdTo());
-		update.setFromDate(updateDetails.getFromDate());
-		update.setCustomeMethodId(updateDetails.getCustomeMethodId());
-		update.setConversationFactories(updateDetails.getConversationFactories());
-		update.setDecimalScale(updateDetails.getDecimalScale());
-		update.setRoundingMode(updateDetails.getRoundingMode());
-		update.setDmucd(updateDetails.getDmucd());
-		update.setDctu(updateDetails.getDctu());
-		return addAllRecords(update);
-
-	}*/
-
 }
+
+
+
+
+
+
